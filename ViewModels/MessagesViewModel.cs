db9b92a609ca7af717a1fd6eefed97f456a2e303
AhmedElhoceny <72575988+AhmedElhoceny@users.noreply.github.com>
2022-03-17 01:23:16 +0200

    Add files via upload

---
+++ ViewModels/MessagesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Chaty.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chaty.ViewModels
+{
+    public class MessagesViewModel
+    {
+        public Clients Client { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
